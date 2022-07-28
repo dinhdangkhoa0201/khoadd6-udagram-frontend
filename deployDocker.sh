853c17a3d57bd2eedee3956c8a1e4b3583352d83
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-07-28 21:55:52 +0700

    Modify deployDocker

--- deployDocker.sh
+++ deployDocker.sh
@@ -1,4 +1,4 @@
 docker build -t udagram-frontend .
-#docker tag udagram-frontend dinhdangkhoa0201/udagram-frontend
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-#docker push dinhdangkhoa0201/udagram-frontend
+docker tag udagram-frontend dinhdangkhoa0201/udagram-frontend
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push dinhdangkhoa0201/udagram-frontend
