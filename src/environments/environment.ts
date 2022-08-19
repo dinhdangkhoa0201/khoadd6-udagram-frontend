8c95b4651ed60b824e2ce2228d3c3bcf9e0936ce
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-08-19 23:41:17 +0700

    Change Api Host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     appName: "Udagram",
-    apiHost: 'http://af4c804d29b57463faaa9ee62cc6f90a-1796243864.us-east-1.elb.amazonaws.com:8080/api/v0'
+    apiHost: 'http://a81a1899df1ab4fef870b5eee482f586-2108031499.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
